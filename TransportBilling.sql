c5ffe4908c53eb6d65f436b996c1d9220a5dbe77
Abhijeet Kumar <43433387+abhijeetkr01@users.noreply.github.com>
2020-08-26 20:18:54 +0530

    DatabaseCreation

---
+++ TransportBilling.sql
@@ -0,0 +1,25 @@
+create database TransportBilling;
+
+use TransportBilling;
+
+CREATE TABLE Vehicle(
+vehicleID INT PRIMARY KEY IDENTITY(100,1) NOT NULL,
+fuelType VARCHAR(1) NOT NULL,
+vehicleMake VARCHAR(50) NOT NULL,
+vehicleType VARCHAR(50) NOT NULL,
+noOfKiloMeters INT,
+ratePerKiloMeter REAL,
+);
+
+CREATE TABLE Invoice(
+invoiceID INT PRIMARY KEY IDENTITY(1001,1) NOT NULL,
+vehicleID INT NOT NULL,
+seatingCapacity INT,
+loadInKG REAL,
+ratePerKG REAL,
+billAmount REAL,
+FOREIGN KEY (vehicleID) REFERENCES Vehicle(vehicleID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+select * from Vehicle;
+select * from Invoice;
