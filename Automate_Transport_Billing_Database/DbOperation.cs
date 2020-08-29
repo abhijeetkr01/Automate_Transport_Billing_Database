2a425223ab7f951c387792b4269ae768b2e2aba7
ABHIJEET <abkr012@gmail.com>
2020-08-29 20:38:08 +0530

    Updated as per 3NF

--- Automate_Transport_Billing_Database/DbOperation.cs
+++ Automate_Transport_Billing_Database/DbOperation.cs
@@ -35,38 +35,30 @@ namespace Automate_Transport_Billing_Database
 
         public static void editVechile(int vID)
         {
+            string vType = getVType(vID);
             try
             {
-                SqlCommand cmd = new SqlCommand("SELECT vehicleType FROM Vehicle where vehicleID = @vehicleID;", con);
-                cmd.Parameters.AddWithValue("@vehicleID", vID);
-                con.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                string vType = null;
-                if (rdr.Read())
-                    vType = rdr["vehicleType"].ToString();
+                if(vType!=null)
+                {
+                    if (vType.Equals("MINI", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        MiniVehicle mini = getInitializeObjMini("MINI");
+                        updateMini(mini, vID);
+                        searchVehicle(vID);
+                    }
+                    else if (vType.Equals("MAXI", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        MaxiVehicle maxi = getInitializeObjMaxi("MAXI");
+                        updateMaxi(maxi, vID);
+                        searchVehicle(vID);
+                    }
+                }
                 else
                     throw new Exception("Vehile with Vehicle ID = " + vID + " not found.");
-
-                con.Close();
-                if(vType=="MINI")
-                {
-                    MiniVehicle mini = getInitializeObjMini("MINI");
-                    updateMini(mini, vID);
-                }
-                else if(vType=="MAXI")
-                {
-                    MaxiVehicle maxi = getInitializeObjMaxi("MAXI");
-                    updateMaxi(maxi, vID);
-                }
-                searchVehicle(vID);
             }
             catch (Exception e)
             {
-                Console.WriteLine("\n"+e.Message);
-            }
-            finally
-            {
-                con.Close();
+                Console.WriteLine("\n" + e.Message);
             }
         }
 
@@ -74,7 +66,7 @@ namespace Automate_Transport_Billing_Database
         {
             try
             {
-                SqlCommand cmd = new SqlCommand("Delete from Vehicle where vehicleID = @vehicleID", con);
+                SqlCommand cmd = new SqlCommand("DELETE FROM Vehicle WHERE vehicleID = @vehicleID", con);
                 cmd.Parameters.AddWithValue("@vehicleID", vID);
                 con.Open();
                 if (cmd.ExecuteNonQuery() == 1)
@@ -96,15 +88,38 @@ namespace Automate_Transport_Billing_Database
         {
             try
             {
-                SqlCommand cmd = new SqlCommand("SELECT veh.*, inv.* from Vehicle veh INNER JOIN Invoice inv on inv.vehicleID = veh.vehicleID  where veh.vehicleID = @vehicleID;", con);
-                cmd.Parameters.AddWithValue("@vehicleID", vID);
-                con.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                displayVehicle(rdr);
+                string vType = getVType(vID);
+                SqlCommand cmd = null;
+                if(vType!=null)
+                {
+                    if (vType.Equals("MINI", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        cmd = new SqlCommand("SELECT Vehicle.*,Bill.billID, Mini.seatingCapacity, (4.5 + (Mini.seatingCapacity - 4)*1)AS ratePerKiloMeter, Bill.billAmount" +
+                                                   " FROM Vehicle INNER JOIN Mini ON Mini.vehicleID = Vehicle.vehicleID INNER JOIN Bill ON Bill.vehicleID = Vehicle.vehicleID WHERE Vehicle.vehicleID = @vehicleID;", con);
+                    }
+                    else if (vType.Equals("MAXI", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        cmd = new SqlCommand("SELECT Vehicle.*,Bill.billID, Maxi.loadInKG," +
+                                                       " CASE WHEN Vehicle.fuelType = 'P' THEN 6.0" +
+                                                       " ELSE 5.0" +
+                                                       " END AS ratePerKiloMeter," +
+                                                       " CASE WHEN Vehicle.vehicleMake = 'ASHOK LEYLAND' THEN 1.5" +
+                                                       " WHEN Vehicle.vehicleMake = 'MAHINDRA' THEN 1.0" +
+                                                       " ELSE 0.5" +
+                                                       " END AS ratePerKG, Bill.billAmount" +
+                                                       " FROM Vehicle INNER JOIN Maxi ON Maxi.vehicleID = Vehicle.vehicleID INNER JOIN Bill ON Bill.vehicleID = Vehicle.vehicleID WHERE Vehicle.vehicleID = @vehicleID;", con);
+                    }
+                    cmd.Parameters.AddWithValue("@vehicleID", vID);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    displayVehicle(rdr);
+                }
+                else
+                    throw new Exception("Vehile with Vehicle ID = " + vID + " not found.");
             }
-            catch (Exception)
+            catch (Exception e )
             {
-                Console.WriteLine("\nVehile with Vehicle ID = " + vID + " not found.");
+                Console.WriteLine("\n" + e.Message);
             }
             finally
             {
@@ -180,13 +195,13 @@ namespace Automate_Transport_Billing_Database
             try
             {
                 mini.validateEntry();
-                SqlCommand cmd = new SqlCommand("INSERT INTO Vehicle VALUES (@fuelType, @vehicleMake, @vehicleType);" +
-                    "INSERT INTO Invoice (vehicleID,noOfKiloMeters,ratePerKiloMeter, seatingCapacity, billAmount) VALUES (SCOPE_IDENTITY(),@noOfKm, @ratePerKM, @seatCap, @billAmount)", con);
+                SqlCommand cmd = new SqlCommand("INSERT INTO Vehicle VALUES (@fuelType, @vehicleMake, @vehicleType, @noOfKM);" +
+                                                "INSERT INTO Mini VALUES(IDENT_CURRENT('Vehicle'), @seatCap);" +
+                                                "INSERT INTO Bill(vehicleID, billAmount) VALUES(IDENT_CURRENT('Vehicle'), @billAmount);", con);
                 cmd.Parameters.AddWithValue("@fuelType", mini.getFuelType());
                 cmd.Parameters.AddWithValue("@vehicleMake", mini.getVehicleMake());
                 cmd.Parameters.AddWithValue("@vehicleType", mini.getVehicleType());
-                cmd.Parameters.AddWithValue("@noOfKm", mini.getNoOfKiloMeters());
-                cmd.Parameters.AddWithValue("@ratePerKM", mini.getRatePerKiloMeter());
+                cmd.Parameters.AddWithValue("@noOfKM", mini.getNoOfKiloMeters());
                 cmd.Parameters.AddWithValue("@seatCap", mini.getSeatingCapacity());
                 cmd.Parameters.AddWithValue("@billAmount", mini.calculateBill());
                 con.Open();
@@ -198,7 +213,7 @@ namespace Automate_Transport_Billing_Database
             catch (Exception e)
             {
                 Console.WriteLine("\nVehicle Not Added.");
-                Console.WriteLine(e.Message);    
+                Console.WriteLine(e.Message);
             }
             finally
             {
@@ -211,15 +226,14 @@ namespace Automate_Transport_Billing_Database
             try
             {
                 maxi.validateEntry();
-                SqlCommand cmd = new SqlCommand("INSERT INTO Vehicle VALUES (@fuelType, @vehicleMake, @vehicleType);" +
-                    "INSERT INTO Invoice (vehicleID,noOfKiloMeters,ratePerKiloMeter,loadInKG, ratePerKG , billAmount) VALUES (SCOPE_IDENTITY(),@noOfKm, @ratePerKM, @load,@ratePerKG, @billAmount)", con);
+                SqlCommand cmd = new SqlCommand("INSERT INTO Vehicle VALUES (@fuelType, @vehicleMake, @vehicleType, @noOfKM);" +
+                                                "INSERT INTO Maxi VALUES(IDENT_CURRENT('Vehicle'), @loadInKG);" +
+                                                "INSERT INTO Bill(vehicleID, billAmount) VALUES(IDENT_CURRENT('Vehicle'), @billAmount);", con);
                 cmd.Parameters.AddWithValue("@fuelType", maxi.getFuelType());
                 cmd.Parameters.AddWithValue("@vehicleMake", maxi.getVehicleMake());
                 cmd.Parameters.AddWithValue("@vehicleType", maxi.getVehicleType());
                 cmd.Parameters.AddWithValue("@noOfKm", maxi.getNoOfKiloMeters());
-                cmd.Parameters.AddWithValue("@ratePerKM", maxi.getRatePerKiloMeter());
-                cmd.Parameters.AddWithValue("@load", maxi.getLoadInKG());
-                cmd.Parameters.AddWithValue("@ratePerKG", maxi.getRatePerKG());
+                cmd.Parameters.AddWithValue("@loadInKG", maxi.getLoadInKG());
                 cmd.Parameters.AddWithValue("@billAmount", maxi.calculateBill());
                 con.Open();
                 if (cmd.ExecuteNonQuery() != 0)
@@ -240,19 +254,19 @@ namespace Automate_Transport_Billing_Database
             }
         }
 
-        static void updateMini(MiniVehicle mini, int vID) 
+        static void updateMini(MiniVehicle mini, int vID)
         {
             try
             {
                 mini.validateEntry();
-                SqlCommand cmd = new SqlCommand("UPDATE Vehicle SET fuelType = @fuelType, vehicleMake= @vehicleMake,vehicleType = @vehicleType where vehicleID = @vehicleID;" +
-                    "UPDATE Invoice SET noOfKiloMeters = @noOfKm,ratePerKiloMeter =@ratePerKM, seatingCapacity=@seatCap, billAmount=@billAmount where vehicleID = @vehicleID", con);
+                SqlCommand cmd = new SqlCommand("UPDATE Vehicle SET fuelType = @fuelType, vehicleMake =@vehicleMake , vehicleType=@vehicleType , noOfKiloMeters =@noOfKM WHERE vehicleID = @vehicleID;" +
+                                                "UPDATE Mini SET seatingCapacity = @seatCap WHERE vehicleID = @vehicleID;" +
+                                                "UPDATE Bill SET billAmount = @billAmount WHERE vehicleID = @vehicleID;", con);
                 cmd.Parameters.AddWithValue("@vehicleID", vID);
                 cmd.Parameters.AddWithValue("@fuelType", mini.getFuelType());
                 cmd.Parameters.AddWithValue("@vehicleMake", mini.getVehicleMake());
                 cmd.Parameters.AddWithValue("@vehicleType", mini.getVehicleType());
-                cmd.Parameters.AddWithValue("@noOfKm", mini.getNoOfKiloMeters());
-                cmd.Parameters.AddWithValue("@ratePerKM", mini.getRatePerKiloMeter());
+                cmd.Parameters.AddWithValue("@noOfKM", mini.getNoOfKiloMeters());
                 cmd.Parameters.AddWithValue("@seatCap", mini.getSeatingCapacity());
                 cmd.Parameters.AddWithValue("@billAmount", mini.calculateBill());
                 con.Open();
@@ -278,26 +292,24 @@ namespace Automate_Transport_Billing_Database
             try
             {
                 maxi.validateEntry();
-                SqlCommand cmd = new SqlCommand("UPDATE Vehicle SET fuelType = @fuelType, vehicleMake= @vehicleMake,vehicleType = @vehicleType where vehicleID = @vehicleID;" +
-                    "UPDATE Invoice SET noOfKiloMeters = @noOfKm,ratePerKiloMeter =@ratePerKM,loadInKG = @load, ratePerKG = @ratePerKG , billAmount=@billAmount where vehicleID = @vehicleID", con);
+                SqlCommand cmd = new SqlCommand("UPDATE Vehicle SET fuelType = @fuelType, vehicleMake = @vehicleMake, vehicleType = @vehicleType, noOfKiloMeters = @noOfKM WHERE vehicleID = @vehicleID;" +
+                                                "UPDATE Maxi SET loadInKG = @loadInKG WHERE vehicleID = @vehicleID;" +
+                                                "UPDATE Bill SET billAmount = @billAmount WHERE vehicleID = @vehicleID;", con);
                 cmd.Parameters.AddWithValue("@vehicleID", vID);
                 cmd.Parameters.AddWithValue("@fuelType", maxi.getFuelType());
                 cmd.Parameters.AddWithValue("@vehicleMake", maxi.getVehicleMake());
                 cmd.Parameters.AddWithValue("@vehicleType", maxi.getVehicleType());
-                cmd.Parameters.AddWithValue("@noOfKm", maxi.getNoOfKiloMeters());
-                cmd.Parameters.AddWithValue("@ratePerKM", maxi.getRatePerKiloMeter());
-                cmd.Parameters.AddWithValue("@load", maxi.getLoadInKG());
-                cmd.Parameters.AddWithValue("@ratePerKG", maxi.getRatePerKG());
+                cmd.Parameters.AddWithValue("@noOfKM", maxi.getNoOfKiloMeters());
+                cmd.Parameters.AddWithValue("@loadInKG", maxi.getLoadInKG());
                 cmd.Parameters.AddWithValue("@billAmount", maxi.calculateBill());
                 con.Open();
-                if (cmd.ExecuteNonQuery() == 1)
+                if (cmd.ExecuteNonQuery() ==3)
                     Console.WriteLine("\nThe Vehicle updated successfully.");
                 else
-                    throw new Exception();
+                    throw new Exception("\nVehicle details not updated.");
             }
             catch (Exception e)
             {
-                Console.WriteLine("\nVehicle details not updated.");
                 Console.WriteLine(e.Message);
             }
             finally
@@ -310,7 +322,8 @@ namespace Automate_Transport_Billing_Database
         {
             try
             {
-                SqlCommand cmd = new SqlCommand("select veh.*, inv.* from Vehicle veh inner join Invoice inv on inv.vehicleID = veh.vehicleID where veh.vehicleType= 'Mini';", con);
+                SqlCommand cmd = new SqlCommand("SELECT Vehicle.*,Bill.billID, Mini.seatingCapacity, (4.5 + (Mini.seatingCapacity - 4)*1)AS ratePerKiloMeter, Bill.billAmount " + 
+                    "FROM Vehicle INNER JOIN Mini ON Mini.vehicleID = Vehicle.vehicleID INNER JOIN Bill ON Bill.vehicleID = Vehicle.vehicleID WHERE Vehicle.vehicleType='MINI';", con);
                 con.Open();
                 SqlDataReader rdr = cmd.ExecuteReader();
                 if (rdr != null)
@@ -338,12 +351,20 @@ namespace Automate_Transport_Billing_Database
                 con.Close();
             }
         }
-   
+
         static void showMaxi()
         {
             try
             {
-                SqlCommand cmd = new SqlCommand("select veh.*, inv.* from Vehicle veh inner join Invoice inv on inv.vehicleID = veh.vehicleID where veh.vehicleType= 'Maxi';", con);
+                SqlCommand cmd = new SqlCommand("SELECT Vehicle.*,Bill.billID, Maxi.loadInKG," +
+                                                " CASE WHEN Vehicle.fuelType = 'P' THEN 6.0" +
+                                                " ELSE 5.0" +
+                                                " END AS ratePerKiloMeter," +
+                                                " CASE WHEN Vehicle.vehicleMake = 'ASHOK LEYLAND' THEN 1.5" +
+                                                " WHEN Vehicle.vehicleMake = 'MAHINDRA' THEN 1.0" +
+                                                " ELSE 0.5" +
+                                                " END AS ratePerKG, Bill.billAmount" +
+                                                " FROM Vehicle INNER JOIN Maxi ON Maxi.vehicleID = Vehicle.vehicleID INNER JOIN Bill ON Bill.vehicleID = Vehicle.vehicleID WHERE Vehicle.vehicleType='MAXI';", con);
                 con.Open();
                 SqlDataReader rdr = cmd.ExecuteReader();
                 if (rdr != null)
@@ -357,7 +378,7 @@ namespace Automate_Transport_Billing_Database
                 }
                 while (rdr.Read())
                 {
-                    fillTable(rdr);  
+                    fillTable(rdr);
                     Console.Write("{0,-16} | ", rdr["loadInKG"]);
                     Console.Write("{0,-11} | ", rdr["ratePerKG"]);
                     Console.Write("{0,-11} | ", rdr["billAmount"]);
@@ -388,7 +409,7 @@ namespace Automate_Transport_Billing_Database
         static void fillTable(SqlDataReader rdr)
         {
             Console.Write("{0,-10} | ", rdr["vehicleID"]);
-            Console.Write("{0,-10} | ", rdr["invoiceID"]);
+            Console.Write("{0,-10} | ", rdr["billID"]);
             Console.Write("{0,-9} | ", rdr["fuelType"]);
             Console.Write("{0,-15} | ", rdr["vehicleMake"]);
             Console.Write("{0,-12} | ", rdr["vehicleType"]);
@@ -402,7 +423,7 @@ namespace Automate_Transport_Billing_Database
             {
                 Console.WriteLine("\n----------The details of Vehicle----------");
                 Console.WriteLine("{0,-20} : {1}", "Vehicle ID", rdr["vehicleID"]);
-                Console.WriteLine("{0,-20} : {1}", "Invoice ID", rdr["invoiceID"]);
+                Console.WriteLine("{0,-20} : {1}", "Invoice ID", rdr["billID"]);
                 Console.WriteLine("{0,-20} : {1}", "Fuel Type", rdr["fuelType"]);
                 Console.WriteLine("{0,-20} : {1}", "Vehicle Make", rdr["vehicleMake"]);
                 Console.WriteLine("{0,-20} : {1}", "Vehicle Type", rdr["vehicleType"]);
@@ -422,6 +443,29 @@ namespace Automate_Transport_Billing_Database
             }
             else
                 throw new Exception();
+        }
+
+        static string getVType(int vID)
+        {
+            string vType = null;
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT vehicleType FROM Vehicle where vehicleID = @vehicleID;", con);
+                cmd.Parameters.AddWithValue("@vehicleID", vID);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.Read())
+                    vType = rdr["vehicleType"].ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return vType;
         }
     }
 }
